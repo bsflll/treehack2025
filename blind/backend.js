8e83f9e9d14909a06b52732745275a8bfe081cae
Rajan Agarwal <rajan.ag005@gmail.com>
2025-02-16 01:59:22 -0800

    phrasing

--- blind/backend.js
+++ blind/backend.js
@@ -9,9 +9,9 @@ dtype: "q4", // optional values: "fp32", "fp16", "q8", "q4", "q4f16"
 device: "cpu", // Optional values: "wasm", "webgpu" (web) or "cpu" (node). If using "webgpu", dtype="fp32" is recommended.
 });
 
-const text = "Starting" ;
+const text = "Person in the way detected. Do you want to report this?" ;
 const audio = await tts.generate(text, {
 // Use tts.list_voices() to list all available voices
 voice: "af_heart", 
 });
-audio.save("starting.wav");
+audio.save("5.wav");
